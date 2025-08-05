a73c1661161a26bddb4d6b8ceb7d1d843055d152
Fassih21 <fassihkamran880@gmail.com>
2025-08-05 22:13:06 +0500

    overwrite

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,7 @@ class User < ApplicationRecord
   has_many :dev_tickets, class_name: 'Ticket', foreign_key: 'dev_id'
   has_many :qa_tickets, class_name: 'Ticket', foreign_key: 'qa_id'
   
-  has_many :tickets, foreign_key: 'user_id', class_name: 'Ticket'
-  has_many :dev_tickets, class_name: 'Ticket', foreign_key: 'dev_id'
-  has_many :qa_tickets, class_name: 'Ticket', foreign_key: 'qa_id'
+
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
