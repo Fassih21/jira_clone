cba8132a6eb7af1bc71c801512ca14baf1bcf30d
Fassih21 <fassihkamran880@gmail.com>
2025-08-05 20:41:14 +0500

    changes

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,6 @@
 class Ticket < ApplicationRecord
-  belongs_to :user
-  belongs_to :dev
-  belongs_to :qa
+  belongs_to :user 
+ 
+  belongs_to :dev, class_name: 'User', foreign_key: 'dev_id'
+  belongs_to :qa,  class_name: 'User', foreign_key: 'qa_id'
 end
