49a382cf77d514930998301d3476c4f88abeae1c
Fassih21 <fassihkamran880@gmail.com>
2025-08-05 21:14:28 +0500

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
- 
-  
+
+  has_many :tickets, class_name: 'Ticket', foreign_key: 'user_id'
+  has_many :dev_tickets, class_name: 'Ticket', foreign_key: 'dev_id'
+  has_many :qa_tickets, class_name: 'Ticket', foreign_key: 'qa_id'
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
