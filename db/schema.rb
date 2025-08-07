576aaa78cc5919d525501aac446f3c378a6d59db
Fassih21 <fassihkamran880@gmail.com>
2025-08-07 16:49:30 +0500

    Migration of Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_07_114147) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_07_114746) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "ticket_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ticket_id"], name: "index_comments_on_ticket_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "tickets", force: :cascade do |t|
     t.string "title"
@@ -42,6 +53,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_07_114147) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "tickets"
+  add_foreign_key "comments", "users"
   add_foreign_key "tickets", "users"
   add_foreign_key "tickets", "users", column: "dev_id"
   add_foreign_key "tickets", "users", column: "qa_id"
