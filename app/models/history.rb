86cdb2861b0857ddaba021c9e066e5531f089754
Fassih21 <fassihkamran880@gmail.com>
2025-08-07 20:05:36 +0500

    Added validations in History Model

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,4 +1,9 @@
 class History < ApplicationRecord
   belongs_to :user
   belongs_to :ticket
+  validates :action, presence: true, inclusion: { in: %w[created updated deleted] }
+  validates :changes, presence: true
+  validates :timestamp, presence: true
+  validates :user_id, presence: true
+  validates :ticket_id, presence: true
 end
