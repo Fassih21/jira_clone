f6849a58ae71a516430848336a397db75e2b1832
Fassih21 <fassihkamran880@gmail.com>
2025-08-07 19:45:21 +0500

    Added data-base level validations

--- db/migrate/20250807114147_create_tickets.rb
+++ db/migrate/20250807114147_create_tickets.rb
@@ -1,9 +1,9 @@
 class CreateTickets < ActiveRecord::Migration[7.1]
   def change
     create_table :tickets do |t|
-      t.string :title
-      t.text :description
-      t.string :status
+      t.string :title, null: false
+      t.text :description, null: false
+      t.string :status, null: false
 
       t.references :user, null: false, foreign_key: true
       t.references :dev, null: false, foreign_key: { to_table: :users }
