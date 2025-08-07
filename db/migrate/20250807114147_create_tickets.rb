964f444af2ecd929bb06d4dd171da47dff78a3b2
Fassih21 <fassihkamran880@gmail.com>
2025-08-07 16:42:08 +0500

    Tickets model created

---
+++ db/migrate/20250807114147_create_tickets.rb
@@ -0,0 +1,14 @@
+class CreateTickets < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tickets do |t|
+      t.string :title
+      t.text :description
+      t.string :status
+      t.references :user, null: false, foreign_key: true
+      t.references :dev, null: false, foreign_key: true
+      t.references :qa, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
