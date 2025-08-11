e4803c19dc88aadf83d5326f0864db87d686a0cf
Fassih21 <fassihkamran880@gmail.com>
2025-08-11 18:37:38 +0500

    Added code in ticket policiy

--- app/policies/ticket_policy.rb
+++ app/policies/ticket_policy.rb
@@ -1,9 +1,42 @@
 class TicketPolicy < ApplicationPolicy
-  # NOTE: Up to Pundit v2.3.1, the inheritance was declared as
-  # `Scope < Scope` rather than `Scope < ApplicationPolicy::Scope`.
-  # In most cases the behavior will be identical, but if updating existing
-  # code, beware of possible changes to the ancestors:
-  # https://gist.github.com/Burgestrand/4b4bc22f31c8a95c425fc0e30d7ef1f5
+
+  def index?
+    user.admin? || user.dev? || user.qa? || user.user?
+  end
+
+  def show?
+    return true if user.admin?
+
+    if user.dev?
+      record.assigned_developer_id == user.id
+    elsif user.qa?
+      record.assigned_qa_id == user.id
+    else
+      record.creator_id == user.id
+    end
+  end
+
+  def create?
+    user.admin? || user.user?
+  end
+
+  def update?
+    user.admin? || user.dev?
+  end
+
+  def assign?
+    user.admin?
+  end
+
+  def mark_done?
+    user.admin? || user.qa?
+  end
+
+  def destroy?
+    user.admin?
+  end
+  end
+
 
   class Scope < ApplicationPolicy::Scope
     # NOTE: Be explicit about which records you allow access to!
