5a2312215db0f7efd0c6c500fd145d157a244b6f
Fassih21 <fassihkamran880@gmail.com>
2025-08-11 14:24:08 +0500

    Added enum in user model for roles..Edited manifest and apllication.html

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :encrypted_password, presence: true
   validates :name, presence: true
   validates :role, presence: true, inclusion: { in: %w[admin user] }
-  
+   enum role: { qa: 2, dev: 1, admin: 1, user: 3 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
