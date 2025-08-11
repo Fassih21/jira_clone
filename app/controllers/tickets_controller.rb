75299174e15a8024c5e45b27547fe17e514bd818
Fassih21 <fassihkamran880@gmail.com>
2025-08-11 20:35:04 +0500

    Added logic in Tickets Controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,90 @@
 class TicketsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_ticket, only: %i[show edit update destroy assign mark_done]
+  after_action :verify_authorized, except: :index
+
+  def index
+    @tickets = case current_user.role
+    when "admin"
+                 Ticket.all
+    when "dev"
+                 Ticket.where(assigned_developer_id: current_user.id)
+    when "qa"
+                 Ticket.where(assigned_qa_id: current_user.id)
+    when "user"
+                 Ticket.where(creator_id: current_user.id)
+    else
+                 Ticket.none
+    end
+  end
+
+  def show
+    authorize @ticket
+  end
+
+  def new
+    @ticket = Ticket.new
+    authorize @ticket
+  end
+
+  def create
+    @ticket = current_user.created_tickets.build(ticket_params)
+    authorize @ticket
+
+    if @ticket.save
+      redirect_to @ticket, notice: "Ticket created successfully."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    authorize @ticket
+  end
+
+  def update
+    authorize @ticket
+
+    if @ticket.update(ticket_params)
+      redirect_to @ticket, notice: "Ticket updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize @ticket
+    @ticket.destroy
+    redirect_to tickets_path, notice: "Ticket deleted successfully."
+  end
+
+  def assign
+    authorize @ticket, :assign?
+
+    if @ticket.update(ticket_params)
+      redirect_to @ticket, notice: "Ticket assigned successfully."
+    else
+      render :edit
+    end
+  end
+
+  def mark_done
+    authorize @ticket, :mark_done?
+
+    if @ticket.update(status: "done")
+      redirect_to @ticket, notice: "Ticket marked as done."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :description, :assigned_developer_id, :assigned_qa_id, :status)
+  end
 end
