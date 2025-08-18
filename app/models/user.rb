31b214a61871f1858729fb97926f5696dc4f48c6
Fassih21 <fassihkamran880@gmail.com>
2025-08-18 15:08:12 +0500

    Added association for functiolaity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-       has_many :created_tickets, class_name: "Ticket", foreign_key: "creator_id"
-       has_many :assigned_developer_tickets, class_name: "Ticket", foreign_key: "developer_id"
+       has_many :created_tickets, class_name: "Ticket", foreign_key: "user_id"
+       has_many :assigned_developer_tickets, class_name: "Ticket", foreign_key: "dev_id"
        has_many :assigned_qa_tickets, class_name: "Ticket", foreign_key: "qa_id"
 
        validates :email, presence: true, uniqueness: true
