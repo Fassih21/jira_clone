5390de60e0ca1d07bdd20b2397838c9d825d87a7
Fassih21 <fassihkamran880@gmail.com>
2025-08-18 21:34:29 +0500

    Edited inclusion in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ApplicationRecord
-       has_many :created_tickets, class_name: "Ticket", foreign_key: "user_id"
-       has_many :assigned_developer_tickets, class_name: "Ticket", foreign_key: "dev_id"
-       has_many :assigned_qa_tickets, class_name: "Ticket", foreign_key: "qa_id"
+  has_many :created_tickets, class_name: "Ticket", foreign_key: "user_id"
+  has_many :assigned_developer_tickets, class_name: "Ticket", foreign_key: "dev_id"
+  has_many :assigned_qa_tickets, class_name: "Ticket", foreign_key: "qa_id"
 
-       validates :email, presence: true, uniqueness: true
-       validates :encrypted_password, presence: true
-       validates :name, presence: true
-       validates :role, presence: true, inclusion: { in: %w[admin user] }
 
-       enum role: { admin: 1, dev: 2, qa: 3, user: 4 }
-       devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  enum role: { admin: 1, dev: 2, qa: 3, user: 4 }
+
+
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+  validates :name, presence: true
+  validates :role, presence: true, inclusion: { in: roles.keys }
 end
