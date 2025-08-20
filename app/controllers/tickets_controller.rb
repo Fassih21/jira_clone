f5b604798660582ac9b14e573caa5463a6957eea
Fassih21 <fassihkamran880@gmail.com>
2025-08-20 16:02:25 +0500

    Updated Controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,10 +1,11 @@
 class TicketsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_ticket, only: %i[show edit update destroy assign mark_done]
+  before_action :set_ticket, only: %i[show edit update destroy mark_done]
   after_action :verify_authorized, except: :index
 
   def index
-        @tickets=policy_scope(Ticket)
+    @tickets = policy_scope(Ticket)
+    @ticket = Ticket.new
   end
 
   def show
@@ -17,13 +18,16 @@ class TicketsController < ApplicationController
   end
 
   def create
-    @ticket = current_user.created_tickets.build(ticket_params)
+    @ticket = Ticket.new(ticket_params)
+    @ticket.creator_id = current_user.id
     authorize @ticket
 
     if @ticket.save
-      redirect_to @ticket, notice: "Ticket created successfully."
+      redirect_to tickets_path, notice: "Ticket created successfully."
     else
-      render :new
+      @tickets = policy_scope(Ticket)
+      flash.now[:alert] = @ticket.errors.full_messages.join(", ")
+      render :index
     end
   end
 
@@ -33,7 +37,6 @@ class TicketsController < ApplicationController
 
   def update
     authorize @ticket
-
     if @ticket.update(ticket_params)
       redirect_to @ticket, notice: "Ticket updated successfully."
     else
@@ -43,13 +46,20 @@ class TicketsController < ApplicationController
 
   def destroy
     authorize @ticket
-   if @ticket.destroy
-    redirect_to tickets_path,
-      notice: "Ticket deleted successfully."
-   else
-    redirect_to ticket_path(@ticket),
-      alert: "Unable to delete Ticket."
-   end
+    if @ticket.destroy
+      redirect_to tickets_path, notice: "Ticket deleted successfully."
+    else
+      redirect_to ticket_path(@ticket), alert: "Unable to delete Ticket."
+    end
+  end
+
+  def mark_done
+    authorize @ticket, :mark_done?
+    if @ticket.update(status: "closed")
+      redirect_to tickets_path, notice: "Ticket marked as done."
+    else
+      redirect_to tickets_path, alert: "Unable to mark ticket as done."
+    end
   end
 
   private
@@ -59,6 +69,11 @@ class TicketsController < ApplicationController
   end
 
   def ticket_params
-    params.require(:ticket).permit(:title, :description, :assigned_developer_id, :assigned_qa_id, :status)
+    params.require(:ticket)
+          .permit(:title, :description, :status, :assigned_developer_id, :assigned_qa_id)
+          .tap do |whitelisted|
+            whitelisted[:dev_id] = whitelisted.delete(:assigned_developer_id) if whitelisted[:assigned_developer_id]
+            whitelisted[:qa_id]  = whitelisted.delete(:assigned_qa_id) if whitelisted[:assigned_qa_id]
+          end
   end
 end
