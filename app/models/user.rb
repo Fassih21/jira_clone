eb11bcdd8efd2cc0120b1ce9b785acaf9984da0a
Fassih21 <fassihkamran880@gmail.com>
2025-08-20 16:05:21 +0500

    Changed Foregin Key in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,13 @@
+# app/models/user.rb
 class User < ApplicationRecord
-  has_many :created_tickets, class_name: "Ticket", foreign_key: "user_id"
-  has_many :assigned_developer_tickets, class_name: "Ticket", foreign_key: "dev_id"
-  has_many :assigned_qa_tickets, class_name: "Ticket", foreign_key: "qa_id"
-
+ has_many :created_tickets, class_name: "Ticket", foreign_key: "creator_id"
+  has_many :assigned_developer_tickets, class_name: "Ticket", foreign_key: "assigned_developer_id"
+  has_many :assigned_qa_tickets, class_name: "Ticket", foreign_key: "assigned_qa_id"
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   enum role: { admin: 1, dev: 2, qa: 3, user: 4 }
-
 
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
