968c42841360e34db46ca4da3cd9d2f2a10eacc6
Fassih21 <fassihkamran880@gmail.com>
2025-08-20 16:04:19 +0500

    Added status in ticket Model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,9 +1,12 @@
 class Ticket < ApplicationRecord
-  belongs_to :creator, class_name: "User", foreign_key: :creator_id
-  belongs_to :dev, class_name: "User", foreign_key: :dev_id, optional: true
-  belongs_to :qa, class_name: "User", foreign_key: :qa_id, optional: true
+  STATUSES = [ "open", "in_progress", "closed" ]
 
+  belongs_to :dev, class_name: "User", foreign_key: "assigned_developer_id", optional: true
+  belongs_to :qa, class_name: "User", foreign_key: "assigned_qa_id", optional: true
+  belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+
+  validates :dev_id, presence: true
   validates :title, presence: true
   validates :description, presence: true
-  validates :status, presence: true, inclusion: { in: %w[open in_progress closed] }
+  validates :status, presence: true, inclusion: { in: STATUSES }
 end
