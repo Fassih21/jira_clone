8425c648099a51d92b7a5866a591a01b6dc5d288
Fassih21 <fassihkamran880@gmail.com>
2025-08-25 16:52:32 +0500

    Added association

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,6 +3,9 @@ class Ticket < ApplicationRecord
   belongs_to :dev, foreign_key: :dev_id, class_name: 'User'
   belongs_to :qa, foreign_key: :qa_id, class_name: 'User'
 
+  has_many :comments, dependent: :destroy
+  has_many :histories, dependent: :destroy
+
   validates :title, presence: true
   validates :description, presence: true  
   validates :status, presence: true, inclusion: { in: %w[open in_progress closed] } 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ApplicationRecord
- validates :email, presence: true, uniqueness: true
+  has_many :created_tickets, class_name: "Ticket", foreign_key: "creator_id", dependent: :nullify
+  has_many :assigned_tickets_as_dev, class_name: "Ticket", foreign_key: "dev_id", dependent: :nullify
+  has_many :assigned_tickets_as_qa, class_name: "Ticket", foreign_key: "qa_id", dependent: :nullify
+
+  has_many :comments, dependent: :destroy
+  has_many :histories, dependent: :destroy
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { admin: "admin", dev: "dev", qa: "qa", user: "user" }
+
+  validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
   validates :name, presence: true
   validates :role, presence: true, inclusion: { in: %w[admin user] }
