757d48cc245545efdb217b9d2e421c8fe3a72861
Fassih21 <fassihkamran880@gmail.com>
2025-08-25 12:35:28 +0500

    Added string instead of integer in enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
-# app/models/user.rb
 class User < ApplicationRecord
- has_many :created_tickets, class_name: "Ticket", foreign_key: "creator_id"
-  has_many :assigned_developer_tickets, class_name: "Ticket", foreign_key: "assigned_developer_id"
-  has_many :assigned_qa_tickets, class_name: "Ticket", foreign_key: "assigned_qa_id"
+  has_many :created_tickets, class_name: "Ticket", foreign_key: "creator_id", dependent: :nullify
+  has_many :assigned_tickets_as_dev, class_name: "Ticket", foreign_key: "dev_id", dependent: :nullify
+  has_many :assigned_tickets_as_qa, class_name: "Ticket", foreign_key: "qa_id", dependent: :nullify
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: { admin: 1, dev: 2, qa: 3, user: 4 }
+  enum role: { admin: "admin", dev: "dev", qa: "qa", user: "user" }
 
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
