1764c33bd9406108e0292302a0d5359c90a4e569
Fassih21 <fassihkamran880@gmail.com>
2025-08-26 13:58:49 +0500

    Matches schema and model

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,9 +1,9 @@
 class History < ApplicationRecord
   belongs_to :user
   belongs_to :ticket
-  validates :action, presence: true, inclusion: { in: %w[created updated deleted] }
-  validates :changes, presence: true
-  validates :timestamp, presence: true
+
+  validates :from_status, presence: true
+  validates :to_status, presence: true
   validates :user_id, presence: true
   validates :ticket_id, presence: true
 end
