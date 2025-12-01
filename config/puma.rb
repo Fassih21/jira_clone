fc9071e6079c0f95a3c010c66bde34d62ec52824
Fassih21 <fassihkamran880@gmail.com>
2025-12-01 20:20:17 +0500

    Updated puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,30 +1,4 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
-#
-# Puma starts a configurable number of processes (workers) and each process
-# serves each request in a thread from an internal thread pool.
-#
-# You can control the number of workers using ENV["WEB_CONCURRENCY"]. You
-# should only set this value when you want to run 2 or more workers. The
-# default is already 1.
-#
-# The ideal number of threads per worker depends both on how much time the
-# application spends waiting for IO operations and on how much you wish to
-# prioritize throughput over latency.
-#
-# As a rule of thumb, increasing the number of threads will increase how much
-# traffic a given process can handle (throughput), but due to CRuby's
-# Global VM Lock (GVL) it has diminishing returns and will degrade the
-# response time (latency) of the application.
-#
-# The default is set to 3 threads as it's deemed a decent compromise between
-# throughput and latency for the average Rails application.
-#
-# Any libraries that use a connection pool or another resource pool should
-# be configured to provide at least as many connections as the number of
-# threads. This includes Active Record's `pool` parameter in `database.yml`.
-# Puma configuration file for production and development
+# Puma configuration for development (Windows) and production (Linux)
 
 # Threads
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
@@ -37,12 +11,13 @@ port ENV.fetch("PORT") { 3000 }
 # Environment
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-# Workers (more concurrency in production)
-# For Render, 2 workers is safe on free/low-tier plans
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+# Workers (cluster mode) — only on non-Windows platforms
+unless Gem.win_platform?
+  workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-# Preload the application for copy-on-write workers
-preload_app!
+  # Preload application for copy-on-write
+  preload_app!
+end
 
 # Restart plugin
 plugin :tmp_restart
