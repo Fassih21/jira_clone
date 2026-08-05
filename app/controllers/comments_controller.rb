a2f1229a2f54eb170f05c0e709c5d336e4c2c4ce
Fassih <fassihkamran880@gmail.com>
2026-08-05 20:28:40 +0530

    Fix: Edited pundit authorization in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
   before_action :set_ticket
 
   def create
+    authorize @ticket, :show? # Ensure the user has permission to view the ticket
     @comment = @ticket.comments.new(comment_params)
     @comment.user = current_user
 
@@ -14,7 +15,9 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    authorize @ticket, :show? # Ensure the user has permission to view the ticket
     @comment = @ticket.comments.find(params[:id])
+    authorize @comment, :destroy? # Ensure the user has permission to destroy the comment
     @comment.destroy
     redirect_to @ticket, notice: "Comment was successfully deleted."
   end
